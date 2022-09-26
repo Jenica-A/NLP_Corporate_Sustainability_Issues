d99c2e59a16c86a367edc7f1200a89fff42ffdab
Jenica-A <95931276+Jenica-A@users.noreply.github.com>
2022-09-26 12:19:56 -0600

    Update wage_disperity.py

--- wage_disperity.py
+++ wage_disperity.py
@@ -13,10 +13,11 @@ from sklearn import linear_model
 
 #Quick linear regression code based on https://www.kaggle.com/code/sarthakniwate13/linear-regression-multiple-variables-example-2
 st.title("Wage Disperity Simulation")
-st.caption('This app uses linear regression to model the difference between employee salary, based on gender')
-st.markdown('''The salary data comes from [this kaggle dataset](https://www.kaggle.com/datasets/fedesoriano/gender-pay-gap-dataset), where a lengthier discussion can be found on the data''',unsafe_allow_html=True)
-st.write("It is stripped down to include only sex, region, years of experience and age. It contains about 21,500 data points")
-st.markdown('''A lengthier discussion can be found in [this 1996 publication](https://docs.iza.org/dp9656.pdf) from the Institute of Labor Economics, Bonn Germany''',unsafe_allow_html=True)
+st.write('This app uses linear regression to model the difference between employee salary, based on gender')
+st.markdown('''The salary data comes from [this kaggle dataset](https://www.kaggle.com/datasets/fedesoriano/gender-pay-gap-dataset), where a lengthier explanation of the data can be found.''',unsafe_allow_html=True)
+st.write("Here, a stripped down version is used for a simple model. Only sex, region, years of experience and age are included.")
+st.write("There are about 21,500 data points.")
+st.markdown('''A lengthier discussion on the topic and data can be found in [this 1996 publication](https://docs.iza.org/dp9656.pdf) from the Institute of Labor Economics, Bonn Germany''',unsafe_allow_html=True)
 
 #@st.cache 
 df = pd.read_csv("./salary_df.csv")
@@ -28,6 +29,8 @@ if st.checkbox("Show raw data"):
     
 st.write('Pick a reasonable amount of experience based on employee age for most reliable model results')
 age = st.slider('Emplyee Age',  min_value=25, max_value=65, step=1)
+st.markdown("***")
+st.markdown("***")
 years_exp = st.slider('Years of Experience',min_value=0,  max_value=50, step=1)
 
 st.write(f"Age = {age} \n\n Year of Experience = {years_exp}")
