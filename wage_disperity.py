74c093cb695a401bfca9d61020f496adc3899b68
Jenica-A <95931276+Jenica-A@users.noreply.github.com>
2022-09-26 13:02:51 -0600

    Update wage_disperity.py

--- wage_disperity.py
+++ wage_disperity.py
@@ -56,7 +56,7 @@ wage_diff = round(float(male_wage - female_wage),2)
 st.subheader("Model Output")
 st.write(f"Female wage is ${female_wage}")
 st.write(f"Male wage is ${male_wage}")
-st.header("Your company's wage gap is ${:,}, annually".format(wage_diff))
+st.header("The wage gap is ${:,}, annually".format(wage_diff))
 
 
 
