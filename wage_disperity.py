0835916e6e46cb5bbe368f32cee39d8f37ef2323
Jenica-A <95931276+Jenica-A@users.noreply.github.com>
2022-09-26 11:22:25 -0600

    Update wage_disperity.py

--- wage_disperity.py
+++ wage_disperity.py
@@ -34,23 +34,23 @@ age = st.slider('age',  min_value=25, max_value=65, step=1)
 years_exp = st.slider('yrsexp',min_value=0,  max_value=50, step=1)
 st.write(f"Age = {age} \n\n Year of Experience = {years_exp}")
     
-X = df[['sex','region','yrsexp','age']]
+X = df[['sex','regions','yrsexp','age']]
 y = df['salary']
 reg = linear_model.LinearRegression()
 reg.fit(X.values,y.values)
 
-section_list = sorted(df.region.unique())
-region = st.selectbox('Choose a region, arbitrarily labeled: 1 = Pacific, 2 = Mountain, 3 = Central, 4 = Eastern',
+section_list = sorted(df.regions.unique())
+regions = st.selectbox('Choose a region (Original data has regions numbered 1.0-4.0; here they arbitrarily labeled: 1 = Pacific, 2 = Mountain, 3 = Central, 4 = Eastern',
      section_list)
 #st.selectbox(label, options, index=0, format_func=special_internal_function, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False, label_visibility="visible")
 
 
-female_wage = reg.predict([[2,region,years_exp,age]])
-male_wage = reg.predict([[1,region,years_exp,age]])
+female_wage = reg.predict([[2,regions,years_exp,age]])
+male_wage = reg.predict([[1,regions,years_exp,age]])
 wage_diff = round(float(male_wage - female_wage),2)
 st.write("female wage is $",female_wage)
 st.write("male wage is $",male_wage)
-st.write(f"In cases where emplyees in region {region}, who are {age} years old, with {years_exp} years of experience, the male worker earns ${wage_diff} more than the female worker annually.")
+st.write(f"In cases where emplyees in region {regions}, who are {age} years old, with {years_exp} years of experience, the male worker earns ${wage_diff} more than the female worker annually.")
 
 
 
