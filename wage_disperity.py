f958cd39f31f5ff6b68ae59bd7fefaea6149d925
Jenica-A <95931276+Jenica-A@users.noreply.github.com>
2022-09-26 12:26:17 -0600

    Update wage_disperity.py

--- wage_disperity.py
+++ wage_disperity.py
@@ -18,6 +18,7 @@ st.markdown("***")
 st.markdown('''The salary data comes from [this kaggle dataset](https://www.kaggle.com/datasets/fedesoriano/gender-pay-gap-dataset), where a lengthier explanation of the data can be found.''',unsafe_allow_html=True)
 st.markdown('''The data replicates [this 1996 publication](https://docs.iza.org/dp9656.pdf) from the Institute of Labor Economics, Bonn Germany''',unsafe_allow_html=True)
 st.write("Here, a stripped down version is used for a simple model. Only sex, region, years of experience and age are included.")
+st.markdown("***")
 
 
 #@st.cache 
@@ -28,22 +29,22 @@ if st.checkbox("Show raw data"):
     st.write("I labeled the regions as: 1 = 'Pacific', 2 = 'Mountain', 3 = 'Central', 4 = 'Eastern'")
     st.write(df)
     
-st.write('Pick a reasonable amount of experience based on employee age for most reliable model results')
+st.caption('Pick a reasonable amount of experience based on employee age, for the most reliable model results')
 age = st.slider('Emplyee Age',  min_value=25, max_value=65, step=1)
 st.markdown("***")
 years_exp = st.slider('Years of Experience',min_value=0,  max_value=50, step=1)
+st.markdown("***")
+section_list = sorted(df.region.unique())
+region = st.selectbox('Choose a region, arbitrarily imagined: 1 = Pacific, 2 = Mountain, 3 = Central, 4 = Eastern',
+     section_list)
 
-st.write(f"Age = {age} \n\n Year of Experience = {years_exp}")
+st.write(f"Age = {age} \n\n Years of Experience = {years_exp} \n\n Region = {region}")
+st.markdown("***")
     
 X = df[['sex','region','yrsexp','age']]
 y = df['salary']
 reg = linear_model.LinearRegression()
 reg.fit(X.values,y.values)
-
-section_list = sorted(df.region.unique())
-region = st.selectbox('Choose a region, arbitrarily imagined: 1 = Pacific, 2 = Mountain, 3 = Central, 4 = Eastern',
-     section_list)
-#st.selectbox(label, options, index=0, format_func=special_internal_function, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False, label_visibility="visible")
 
 
 female_wage = np.round(reg.predict([[2,region,years_exp,age]]),2)
